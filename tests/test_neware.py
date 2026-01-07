5b503a3281bd25f3fd51d16fd4432e7071e6d1cd
Graham Kimbell <88666181+g-kimbell@users.noreply.github.com>
2026-01-07 13:05:36 +0100

    More Neware tests

--- tests/test_neware.py
+++ tests/test_neware.py
@@ -2,8 +2,10 @@
 
 from __future__ import annotations
 
+from pathlib import Path
 from xml.etree.ElementTree import Element
 
+import pytest
 from defusedxml import ElementTree
 
 from aurora_unicycler import (
@@ -14,8 +16,19 @@ from aurora_unicycler import (
     Protocol,
     RecordParams,
     SafetyParams,
+    SampleParams,
+    Step,
     Tag,
 )
+
+
+def _get_value(node: Element, path: str) -> str:
+    """Get "Value" attributes from xml, asserting values exist."""
+    new_node = node.find(path)
+    assert new_node is not None
+    val = new_node.get("Value")
+    assert val is not None
+    return val
 
 
 def test_to_neware_xml(test_data: dict) -> None:
@@ -44,7 +57,7 @@ def test_to_neware_xml(test_data: dict) -> None:
     assert len(step_info) == int(step_info.attrib["Num"])
 
 
-def test_tag_neware(test_data: dict) -> None:
+def test_tag_neware() -> None:
     """Test tags in Neware XML."""
     protocol = Protocol(
         record=RecordParams(time_s=1),
@@ -62,10 +75,8 @@ def test_tag_neware(test_data: dict) -> None:
     neware_ET = ElementTree.fromstring(xml_string)
     loopstep = neware_ET.find("config/Step_Info/Step5")
     assert loopstep is not None
-    assert loopstep.attrib["Step_Type"] == "5"
-    loop_to = loopstep.find("Limit/Other/Start_Step")
-    assert loop_to is not None
-    assert loop_to.attrib["Value"] == "3"
+    assert loopstep.get("Step_Type") == "5"
+    assert _get_value(loopstep, "Limit/Other/Start_Step") == "3"
 
     protocol1 = Protocol(
         record=RecordParams(time_s=1),
@@ -125,12 +136,8 @@ def test_cv_neware(test_data: dict) -> None:
     xml = protocol.to_neware_xml(sample_name="test", capacity_mAh=5)
     step3 = ElementTree.fromstring(xml).find("config/Step_Info/Step3/Limit/Main")
     assert isinstance(step3, Element)
-    rate = step3.find("Rate")
-    assert isinstance(rate, Element)
-    assert float(rate.get("Value")) == 0.1
-    curr = step3.find("Curr")
-    assert isinstance(curr, Element)
-    assert float(curr.get("Value")) == 0.5
+    assert float(_get_value(step3, "Rate")) == 0.1
+    assert float(_get_value(step3, "Curr")) == 0.5
 
     protocol = Protocol(
         record=RecordParams(time_s=1),
@@ -145,8 +152,67 @@ def test_cv_neware(test_data: dict) -> None:
     xml = protocol.to_neware_xml(sample_name="test", capacity_mAh=1)
     step3 = ElementTree.fromstring(xml).find("config/Step_Info/Step3/Limit/Main")
     assert isinstance(step3, Element)
-    rate = step3.find("Rate")
-    assert rate is None
-    curr = step3.find("Curr")
-    assert isinstance(curr, Element)
-    assert float(curr.get("Value")) == 0.5
+    assert step3.find("Rate") is None
+    assert float(_get_value(step3, "Curr")) == 0.5
+
+
+def test_safety() -> None:
+    """Ensure safety settings are applied."""
+    protocol = Protocol(
+        sample=SampleParams(name="test"),
+        record=RecordParams(time_s=1),
+        safety=SafetyParams(
+            max_voltage_V=5.0,
+            min_voltage_V=0.0,
+            max_current_mA=10.0,
+            min_current_mA=-10.0,
+            max_capacity_mAh=5.0,
+        ),
+        method=[
+            OpenCircuitVoltage(until_time_s=1),
+        ],
+    )
+
+    safety = ElementTree.fromstring(protocol.to_neware_xml()).find("config/Whole_Prt/Protect/Main")
+    assert safety is not None
+
+    assert float(_get_value(safety, "Volt/Upper")) == 50000.0
+    assert float(_get_value(safety, "Volt/Lower")) == 0.0
+    assert float(_get_value(safety, "Curr/Upper")) == 10.0
+    assert float(_get_value(safety, "Curr/Lower")) == -10.0
+    assert float(_get_value(safety, "Cap/Upper")) == 5.0 * 3600
+
+
+def test_unknown_step() -> None:
+    """If unsupported steps are in protocol, raise error."""
+
+    class UnknownStep(Step):
+        step: str = "wait, what"
+
+    protocol = Protocol.model_construct(
+        record=RecordParams(time_s=1),
+        method=[UnknownStep()],
+    )
+    with pytest.raises(NotImplementedError) as excinfo:
+        protocol.to_neware_xml(sample_name="test")
+    assert "to_neware_xml() does not support step type: wait, what" in str(excinfo.value)
+
+
+def test_save_file(tmpdir: Path) -> None:
+    """Check file is written correctly."""
+    protocol = Protocol(
+        record=RecordParams(time_s=1),
+        method=[
+            Tag(tag="a"),
+            ConstantCurrent(current_mA=0.01, until_voltage_V=4),
+            ConstantVoltage(voltage_V=4, until_time_s=1),
+            ConstantCurrent(current_mA=0.01, until_voltage_V=3),
+            Loop(loop_to="a", cycle_count=100),
+        ],
+    )
+    filepath = Path(tmpdir / "test.xml")
+    res = protocol.to_neware_xml(sample_name="test", save_path=filepath)
+    assert filepath.exists()
+    with (tmpdir / "test.xml").open("r") as f:
+        text = f.read()
+    assert res == text
